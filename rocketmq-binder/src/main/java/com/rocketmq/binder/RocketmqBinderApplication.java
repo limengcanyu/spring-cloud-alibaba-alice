c8afa11f7733d1cc2b06bb1106a447b3ce2de8ba
limengcanyu <limengcanyu@163.com>
2019-11-24 07:42:58 +0800

    add RocketMQ Binder

---
+++ rocketmq-binder/src/main/java/com/rocketmq/binder/RocketmqBinderApplication.java
@@ -0,0 +1,13 @@
+package com.rocketmq.binder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqBinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqBinderApplication.class, args);
+    }
+
+}
---
+++ rocketmq-binder/src/test/java/com/rocketmq/binder/RocketmqBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketmq.binder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqBinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
